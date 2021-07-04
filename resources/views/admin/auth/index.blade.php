4c8678bf2184062966e9025e4c83eba44e1fe9b9
Naba <11944066+Naba0123@users.noreply.github.com>
2021-07-04 21:38:34 +0900

    AdminLTE 導入

--- resources/views/admin/auth/index.blade.php
+++ resources/views/admin/auth/index.blade.php
@@ -1,14 +1,69 @@
-@extends('admin._share.layout')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
 
-@section('content')
-    <form action="{{ route('admin.auth.login') }}" method="post">
-        <label>
-            <input type="email" name="email" />
-        </label>
-        <label>
-            <input type="password" name="password"/>
-        </label>
-        @csrf
-        <input type="submit" name="ログイン">
-    </form>
-@endsection
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ url('vendor/adminlte/plugins/fontawesome-free/css/all.min.css') }}">
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet" href="{{ url('vendor/adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ url('vendor/adminlte/dist/css/adminlte.min.css') }}">
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+    <!-- /.login-logo -->
+    <div class="card card-outline card-primary">
+        <div class="card-header text-center">
+            <p class="h1"><b>{{ config('blog.common.title') }}</b></p>
+        </div>
+        <div class="card-body">
+
+            @if (session('error'))
+                <div class="callout callout-danger">
+                    <h5>I am a danger callout!</h5>
+
+                    <p>There is a problem that we need to fix. A wonderful serenity has taken possession of my entire
+                        soul,
+                        like these sweet mornings of spring which I enjoy with my whole heart.</p>
+                </div>
+            @endif
+
+            <form action="{{ route('admin.auth.login') }}" method="post">
+                @csrf
+                <div class="input-group mb-3">
+                    <input name="email" type="email" class="form-control" placeholder="Email">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-envelope"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="input-group mb-3">
+                    <input name="password" type="password" class="form-control" placeholder="Password">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-lock"></span>
+                        </div>
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary btn-block">{{ __('admin.auth.login_button') }}</button>
+            </form>
+        </div>
+    </div>
+</div>
+
+<!-- jQuery -->
+<script src="{{ url('vendor/adminlte/plugins/jquery/jquery.min.js') }}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{ url('vendor/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<!-- AdminLTE App -->
+<script src="{{ url('vendor/adminlte/dist/js/adminlte.min.js') }}"></script>
+
+</body>
+</html>
