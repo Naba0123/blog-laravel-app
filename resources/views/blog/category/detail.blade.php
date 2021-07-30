6f63fc2e5d7abeacdbc1a09d0ff8a082dcc581f5
Naba <11944066+Naba0123@users.noreply.github.com>
2021-07-30 21:12:03 +0900

    レイアウト調整

--- resources/views/blog/category/detail.blade.php
+++ resources/views/blog/category/detail.blade.php
@@ -1,14 +1,15 @@
 @extends('blog._share.layout', ['title' => $category->name])
 
 @section('content')
+
     <div class="lb-content-header">
         <h2><i class="fa fa-tag"></i> {{ $category->name }}</h2>
     </div>
 
-    <div class="lb-article-body">
-        @foreach ($category->articles as $article)
+    @foreach ($category->articles as $article)
+        <div class="mt-5 mb-5 lb-box-shadow-gray">
             @include('blog._parts.list_article', ['article' => $article])
-        @endforeach
-    </div>
+        </div>
+    @endforeach
 
 @endsection
