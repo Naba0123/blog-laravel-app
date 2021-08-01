d7b6fa33cb58afc963e692af9102f1b98e3d428d
Naba <11944066+Naba0123@users.noreply.github.com>
2021-08-01 12:54:02 +0900

    関連記事が死んでいるのを修正

--- resources/views/blog/_parts/related_articles.blade.php
+++ resources/views/blog/_parts/related_articles.blade.php
@@ -7,7 +7,7 @@
         @foreach($articles as $article)
             <div class="col-sm-6">
                 <div class="mt-3 mb-3 lb-box-shadow-lightgray">
-                    @include('blog._parts.list_article', ['article' => $article])
+                    @include('blog._parts.article_card', ['article' => $article])
                 </div>
             </div>
         @endforeach
